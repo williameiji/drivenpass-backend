8d5d1140f86e1ded674cef2542e34c5d76a38dc9
williameiji <william.fujimori@gmail.com>
2022-09-15 20:20:34 -0300

    fix: thunder-collection

--- src/middlewares/validators/errorHandler.ts
+++ src/middlewares/validators/errorHandler.ts
@@ -1,18 +1,16 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 
 export default async function errorHandler(
 	error: any,
 	req: Request,
 	res: Response,
-	next: NextFunction
+	next: any
 ) {
 	if (error.code === "BadRequest") return res.status(400).send(error.message);
 	if (error.code === "WrongType") return res.status(422).send(error.message);
 	if (error.code === "Anauthorized") return res.status(401).send(error.message);
 	if (error.code === "NotFound") return res.status(404).send(error.message);
 	if (error.code === "Conflict") return res.status(409).send(error.message);
-
-	console.log(error);
 
 	return res.sendStatus(500);
 }
